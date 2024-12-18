8225f8d5339e0deb806e0616e752e50850a1c5a4
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-12-18 15:13:00 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+/*
+Ampliare l'esercizio precedente aggiungendo, nel form, i campi per immagine, contenuto, categoria (select), tags (lista di checkbox) e uno stato per pubblicare o meno l'articolo.
+Utilizzare un unico oggetto per gestire tutti i dati del form.
+BONUS:
+1. Aggiungere uno useEffect che mostri un alert quando l’utente clicca sull’apposita checkbox per pubblicare un articolo.
+*/
+
+
+
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
